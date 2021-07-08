2c4b7832c9fce45a579fa59b5e9bfe7fcc22adfc
Mari Vaypan <avemari98@gmail.com>
2021-07-08 04:42:41 +0300

    added another content of form

---
+++ productsData.js
@@ -0,0 +1,47 @@
+const productsList = [
+    {
+        id: 2,
+        htmlData: `
+        <h3 class="form__point point-delete">Product 2 &#160;<a href="#"></a></h3>
+    <p class="form__input-text">Enter main keyword for the product</p>
+    <input type="email" class="form__input" placeholder="for example, sylicon wine cup">
+    <p class="form__input-text">Enter link to the similar product as a reference</p>
+    <input type="text" class="form__input" placeholder="https://...">
+        `
+    },
+    {
+        id: 3,
+        htmlData: `
+        <h3 class="form__point point-delete">Product 3 &#160;<a href="#"></a></h3>
+    <p class="form__input-text">Enter main keyword for the product</p>
+    <input type="email" class="form__input" placeholder="for example, sylicon wine cup">
+    <p class="form__input-text">Enter link to the similar product as a reference</p>
+    <input type="text" class="form__input" placeholder="https://...">
+        `
+    },
+    {
+        id: 4,
+        htmlData: `
+        <h3 class="form__point point-delete">Product 4 &#160;<a href="#"></a></h3>
+    <p class="form__input-text">Enter main keyword for the product</p>
+    <input type="email" class="form__input" placeholder="for example, sylicon wine cup">
+    <p class="form__input-text">Enter link to the similar product as a reference</p>
+    <input type="text" class="form__input" placeholder="https://...">
+        `
+    },
+    {
+        id: 5,
+        htmlData: `
+        <h3 class="form__point point-delete">Product 5 &#160;<a href="#"></a></h3>
+    <p class="form__input-text">Enter main keyword for the product</p>
+    <input type="email" class="form__input" placeholder="for example, sylicon wine cup">
+    <p class="form__input-text">Enter link to the similar product as a reference</p>
+    <input type="text" class="form__input" placeholder="https://...">
+        `
+    }
+]
+
+
+
+//delete product
+//compain products 
